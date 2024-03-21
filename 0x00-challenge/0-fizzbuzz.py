4fb8668cf52291aee19ce166761ad2eed730f7e9
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-21 09:37:58 +0000

    task0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 FizzBuzz functiom prints from 1 to n separated by space.
 - For multiples of 3 it pronts "Fizz" instead of the number.
@@ -28,7 +28,7 @@ def fizzbuzz(n):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
