7186c96837ff4c273b4f99d24e5abb4d2a0d5fcc
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-21 09:30:53 +0000

    task 0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-FizzBuzz
+FizzBuzz functiom prints from 1 to n separated by space.
 """
 
 import sys
@@ -21,10 +21,10 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
