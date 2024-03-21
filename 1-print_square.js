4c6da5f41e472ea30ccdb6cf1bb381be3b2ce69a
Eseoghene-ODODO <en.ododo@gmail.com>
2024-03-21 02:57:24 +0000

    task 1

--- 1-print_square.js
+++ 1-print_square.js
@@ -7,7 +7,8 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2)
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
@@ -16,8 +17,10 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2]);
 
-for (let i = 0; i < size; i ++) {
-    for (let j = 0; j < size; j ++) {
+for (let i = 0; i < size; i ++)
+{
+    for (let j = 0; j < size; j ++)
+	{
         process.stdout.write("#");
     }
     process.stdout.write("\n");
